e153f782ff757b3f84da0dcf43c7d4481a04ac62
leeooh <leeooh.lopez@gmail.com>
2016-04-09 16:18:40 -0500

    reorganizing

--- plot1.R
+++ plot1.R
@@ -1,10 +1,12 @@
 #reading the data
 dataFile <- "./household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, 
+                   dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #subsetting and plotting
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(globalActivePower, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
 dev.off()
